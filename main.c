960cb90c094d440b30af9e6d024d9e85de31321e
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-08-10 08:35:44 -0500

    Conflico con un errro desconocido

--- main.c
+++ main.c
@@ -63,6 +63,52 @@ int Ins_Next_Element(List *list, Element *element, char *Path)
 	(list->Size_List)++;
 	return (0);
 }
+
+int Rem_Nex_Element(List *list, Element *element, char **Path)
+{
+	Element *Old_Element;
+
+	if (!(list->Size_List))
+		return (0);
+
+	if (element == NULL)
+	{
+		*Path       = list->Head->Path;
+		Old_Element = list->Head;
+		list->Head  = list->Head->Next;
+	}
+
+	if (list->Size_List == 1)
+	{
+		list->Tail = NULL;
+	}
+	else
+	{
+		if (element->Next == NULL)
+			return -1;
+
+		*Path         = element->Path;
+		Old_Element   = element->Next;
+		element->Next = element->Next->Next;
+
+		if (element->Next == NULL)
+			list->Tail = element;
+	}
+
+	free (Old_Element);
+
+	(list->Size_List)--;
+	return (0);
+}
+void List_Destroy(List *list)
+{
+	char *Path;
+
+	while (list->Size_List > 0)
+		if ((Rem_Nex_Element(list, NULL, &Path) == 0) && (list->Destroy != NULL))
+			list->Destroy(Path);
+	return ;
+}
 void
 Get_Path(List *list)
 {
@@ -70,6 +116,14 @@ Get_Path(List *list)
 	Posicionar_Environ(environ);
 	while(**environ)
 		Ins_Next_Element(list, NULL,Data_Path());
+	printf("Eliminamos la estructura de datos");
+	List_Destroy(list);
+//	printf("Tamaño de la lista = %d\n", list->Size_List);
+}
+
+void Destroy(char *PATH)
+{
+	free(PATH);
 }
 
 int
@@ -77,10 +131,6 @@ main(void)
 {
 	List list;
 
-	/*
-	 *   Inicializamos la estructura.
-	 */
-
-	List_Init(&list, NULL, NULL);
+	List_Init(&list, Destroy, NULL);
 	Get_Path(&list);
 }
