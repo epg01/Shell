9c6685c2ec42b090ccb794bcaaedaa9b42d552b0
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-08-10 07:51:16 -0500

    Ya tenemos la estructura de datos hecha

--- main.c
+++ main.c
@@ -45,8 +45,6 @@ int Ins_Next_Element(List *list, Element *element, char *Path)
 		return (-1);
 	New_Element->Path = Path;
 	Length_PATH(New_Element, (New_Element->Length_Path));
-	printf("Path:\n%s\n", New_Element->Path);
-	printf("Length:\n%i\n", New_Element->Length_Path);
 	if (!element)
 	{
 		if (!(list->Tail))
@@ -63,25 +61,15 @@ int Ins_Next_Element(List *list, Element *element, char *Path)
 	}
 
 	(list->Size_List)++;
-	printf("Se creo el nodo\n");
-	sleep(4);
 	return (0);
 }
 void
 Get_Path(List *list)
 {
 	Posicionar_Path(environ);
-
-	printf("La variable environ esta en el path\n");
-	printf("%s\n", *environ);
-	printf("movemos la variable envirn para a puntar al \\\n");
 	Posicionar_Environ(environ);
-	printf("%s\n", *environ);
-	printf("Procedemos a ingresar las rutas en la estructura de datos\n");
-
 	while(**environ)
 		Ins_Next_Element(list, NULL,Data_Path());
-	printf("Se creo la base de datos, ahora procedemos a imprimir dichas rutas\n");
 }
 
 int
